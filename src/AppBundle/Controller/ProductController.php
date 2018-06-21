78f885626d92e5841d69eb79661fd0b2b6ee9bee
aymen <mwb_aymen@hotmail.fr>
2018-06-21 18:49:22 +0200

    add new controllers api

---
+++ src/AppBundle/Controller/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as REST;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @REST\RouteResource("Product")
+ * @REST\NamePrefix("api_")
+ */
+class ProductController extends FOSRestController
+{
+
+    public function cgetAction()
+    {
+        return new Response('yeees');
+        // replace this example code with whatever you need
+        /*return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);*/
+    }
+}
